20e2a3789fbaad185fc6712dae0e457562bb1333
Lakshya Upreti <upretiluv06@gmail.com>
2025-12-14 15:44:20 +0530

    Thirsty Code

--- hungry.py
+++ hungry.py
@@ -2,4 +2,7 @@ hungry = input("Are you hungry?: Y/N")
 if hungry == "Y":
     print("Hungry")
 elif hungry == "N":
-    print("Not hungry")
+    thirsty = input("Are you thirsty?: Y/N")
+    if thirsty == "Y":
+        print("Drink Water.")
+
